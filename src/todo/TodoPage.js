03c211e0f2c8dd830eb709180b96b208c75d236e
Chase Abbott <cabbott93@gmail.com>
2021-05-17 16:20:52 -0700

    working component did mount

--- src/todo/TodoPage.js
+++ src/todo/TodoPage.js
@@ -1,12 +1,18 @@
 import { Component } from 'react';
 import './TodoPage.css';
-import { addTodo } from '../utils/todo-api.js';
+import { addTodo, getTodos } from '../utils/todo-api.js';
 
 export default class TodoPage extends Component {
   state = {
     todos: [],
     task: '',
   };
+
+  componentDidMount = async () => {
+    const updatedTodos = await getTodos();
+    console.log(updatedTodos);
+    this.setState({ todos: updatedTodos });
+  }
 
   handleNewTodo = ({ target }) => {
     this.setState({ task: target.value });
@@ -16,13 +22,17 @@ export default class TodoPage extends Component {
     e.preventDefault();
     const { task, todos } = this.state;
     try {
-      const todo = await addTodo({
-        task: task,
-        completed: false,
-        shared: false,
-      });
-      const updatedTodos = [...todos, todo];
-      this.setState({ task: '', todos: updatedTodos });
+      if (task) {
+        const todo = await addTodo({
+          task: task,
+          completed: false,
+          shared: false,
+        });
+        const updatedTodos = [...todos, todo];
+        this.setState({ task: '', todos: updatedTodos });
+      } else {
+        console.log('error: invalid todo');
+      }
     } catch (err) {
       console.log(err.message);
     }
