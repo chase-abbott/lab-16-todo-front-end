fff91985842bca9c374dcfde8520274bdaa174ef
thankrandomness <vijaybchakilam@gmail.com>
2021-05-17 17:26:54 -0400

    signup and signin routes

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -1,83 +1,110 @@
 import { Component } from 'react';
+import { signUp, signIn } from '../../utils/todo-api.js';
 import './Auth.css';
 
 export default class Auth extends Component {
   state = {
-    isSignup: true,
+    isSignUp: true,
     name: '',
     email: '',
-    password: ''
-  }
+    password: '',
+  };
 
-  handleSubmit = e => {
+  handleSubmit = async (e) => {
+    const { isSignUp } = this.state;
     e.preventDefault();
-  }
+
+    try {
+      const action = isSignUp ? signUp : signIn;
+
+      const user = await action(this.state);
+      console.log(user);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   handleNameChange = ({ target }) => {
     this.setState({ name: target.value });
-  }
+  };
 
   handleEmailChange = ({ target }) => {
     this.setState({ email: target.value });
-  }
+  };
 
   handlePasswordChange = ({ target }) => {
     this.setState({ password: target.value });
-  }
+  };
 
   handleSignUp = () => {
-    this.handleSubmit();
-  }
+    this.handleSubmit(this.state);
+  };
 
   handleSignIn = () => {
-    this.handleSubmit();
-  }
+    this.handleSubmit(this.state);
+  };
 
-  handleSignUpToggle = e => {
+  handleSignUpToggle = (e) => {
     e.preventDefault();
-    this.setState({ isSignup: !this.state.isSignup });
-  }
+    this.setState({ isSignUp: !this.state.isSignUp });
+  };
 
   render() {
-
     return (
-      <div className="Auth">
+      <div className='Auth'>
         <form onSubmit={this.handleSubmit}>
-          {this.state.isSignup &&
+          {this.state.isSignUp && (
             <p>
               <label>
                 Name
-                <input value={this.state.name} onChange={this.handleNameChange}></input>
+                <input
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                ></input>
               </label>
             </p>
-          }
+          )}
 
           <p>
             <label>
               Email
-              <input value={this.state.email} onChange={this.handleEmailChange}></input>
+              <input
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+              ></input>
             </label>
           </p>
 
           <p>
             <label>
               Password
-              <input value={this.state.password} type='password' onChange={this.handlePasswordChange}></input>
+              <input
+                value={this.state.password}
+                type='password'
+                onChange={this.handlePasswordChange}
+              ></input>
             </label>
           </p>
 
-          {this.state.isSignup
-            ? <button onClick={this.handleSignUp}> Sign Up </button>
-            : <button onClick={this.handleSignIn}> Sign In </button>
-          }
+          {this.state.isSignUp ? (
+            <button> Sign Up </button>
+          ) : (
+            <button> Sign In </button>
+          )}
 
-          {this.state.isSignup
-            ? <button onClick={this.handleSignUpToggle}> Already have an account? </button>
-            : <button onClick={this.handleSignUpToggle}> Need to create account? </button>
-          }
+          {this.state.isSignUp ? (
+            <button onClick={this.handleSignUpToggle}>
+              {' '}
+              Already have an account?{' '}
+            </button>
+          ) : (
+            <button onClick={this.handleSignUpToggle}>
+              {' '}
+              Need to create account?{' '}
+            </button>
+          )}
         </form>
       </div>
     );
   }
-
 }
--- src/app/todo-api.js
+++
@@ -1,5 +0,0 @@
-import request from 'superagent';
-
-export function signUp(credentials) {
-
-};
