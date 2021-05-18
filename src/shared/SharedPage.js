4811fc7b372d5e3c04ead6f1330c00c679a1ba14
thankrandomness <vijaybchakilam@gmail.com>
2021-05-18 13:13:32 -0400

    display shared todos successful

--- src/shared/SharedPage.js
+++ src/shared/SharedPage.js
@@ -3,26 +3,28 @@ import { getSharedTodos } from '../utils/todo-api.js';
 import './SharedPage.css';
 
 export default class SharedPage extends Component {
-state = {
-  todos: []
-}
+  state = {
+    todos: [],
+  };
 
-componentDidMount = async () => {
-  const response = await getSharedTodos();
-  this.setState({ todos: response.body });
-}
-  
-render() {
-  const { todos } = this.state;
-  return (
-    <div className="SharedPage">
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}> {todo.task} </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+  componentDidMount = async () => {
+    const todos = await getSharedTodos();
+    this.setState({ todos: todos });
+  };
 
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className='SharedPage'>
+        <div>
+          <h1>Shared Todos</h1>
+        </div>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}> {todo.task} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
